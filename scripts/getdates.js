dbbdd37704e7b3b9b6839fe8c88a31420ccfa06a
gavinjanda <jan22009@byui.edu>
2024-09-20 23:17:51 -0600

    finished assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+const year = document.querySelector("#year");
+
+const today = new Date();
+
+year.innerHTML = `<span>${today.getFullYear()}</span>`;
+
+const lastModified = document.querySelector("#lastModified");
+
+const lastModifiedDate = document.lastModified;
+
+lastModified.innerHTML = `Last Modification: ${lastModifiedDate}`
