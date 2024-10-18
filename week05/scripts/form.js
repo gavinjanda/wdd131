737bb60a4a8c9e09c38983e9012f7891cb6082d0
gavinjanda <jan22009@byui.edu>
2024-10-18 16:55:39 -0600

    moved form location

--- week05/scripts/form.js
+++
@@ -1,9 +0,0 @@
-
-
-// Footer content
-const year = document.querySelector("#year");
-const today = new Date();
-year.innerHTML = `<span>${today.getFullYear()}</span>`;
-const lastModified = document.querySelector("#lastModified");
-const lastModifiedDate = document.lastModified;
-lastModified.innerHTML = `Last Modification: ${lastModifiedDate}`
