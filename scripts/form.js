f2ccba10a7fbfd103da2e547d54fda6674ef4826
gavinjanda <jan22009@byui.edu>
2024-10-19 14:06:32 -0600

    week05 assignment

--- scripts/form.js
+++ scripts/form.js
@@ -26,7 +26,41 @@ const products = [
     }
   ];
 
-  
+//Create option list
+document.addEventListener('DOMContentLoaded', function() {
+  createProduct();
+
+  function createProduct(){
+    products.forEach(product => {
+      const option = document.createElement('option');
+      option.value = product.id;
+      option.textContent = product.name.charAt(0).toUpperCase() + product.name.slice(1);
+      document.querySelector('#product').append(option);
+    })
+  }
+});
+
+if (!localStorage.getItem("clickCount")) {
+  localStorage.setItem("clickCount", 0);
+}
+
+// Add event listener to the submit button on the first page
+const submitButton = document.getElementById("submitButton");
+if (submitButton) {
+  submitButton.addEventListener("click", () => {
+    let reviewNum = parseInt(localStorage.getItem("clickCount")) || 0;
+    reviewNum++;
+    localStorage.setItem("clickCount", reviewNum);
+  });
+}
+
+// Update the span with the click count on the second page
+const reviewsSpan = document.querySelector(".numReviews");
+if (reviewsSpan) {
+  const clickCount = localStorage.getItem("clickCount") || 0;
+  reviewsSpan.textContent = `${clickCount}`;
+}
+
 
 // Footer content
 const year = document.querySelector("#year");
